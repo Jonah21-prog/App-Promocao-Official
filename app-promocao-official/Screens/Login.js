07596c943f7688d02a8a37fae8e7ba24b7d88901
Jonah21-prog <coelhopirigoso@gmail.com>
2021-12-19 17:45:31 -0300

    Testando configuracoes

--- app-promocao-official/Screens/Login.js
+++ app-promocao-official/Screens/Login.js
@@ -19,7 +19,7 @@ export default function Login({ navigation }) {
     <View style={styles.container}>
       <Text h3>Entre aqui</Text>
       <Input
-        placeholder="E-mail"
+        placeholder="E-mail adsfasdfsadfasdf"
         leftIcon={{ type: "font-awesome", name: "envelope" }}
         onChangeText={(value) => setEmail(value)}
         keyboardType="email-address"
